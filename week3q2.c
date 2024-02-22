766d91a94c0b2bcfdeb4d7d4e895c1e7811f0d88
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-02-22 18:43:35 +0530

    Update week3q2.c

--- week3q2.c
+++ week3q2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int is_prime(int n){
+  int count=1;
+  for(int i=2;i<=(n/2);i++){
+    if(n%i==0)
+      return 0;
+  }
+}
+
+int main() {
+
+  int n,num;
+  scanf("%d", &n);
+
+  for(int i=0;i<n;i++){
+      scanf("%d",&num);
+      if(is_prime(num)){
+          printf("%d ",num);
+      }
+  }
+
+  return 0;
+}
