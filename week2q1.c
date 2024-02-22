495d94d5c40b11ffde30db07b861a8cb9b782818
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-02-22 18:41:23 +0530

    Update week2q1.c

--- week2q1.c
+++ week2q1.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+#include<string.h>
+void main(){
+  char str[30];
+  scanf("%[^\n]s",str);
+  int i=0;
+  int count=0;
+  while(*(str+i)!='\0'){
+    if(*(str+i)=='1')
+      count++;
+    i++;
+  }
+  if(count%2==0)
+    printf("0");
+  else
+    printf("1");
+  
+}
