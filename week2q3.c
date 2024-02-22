a78dcea4a111af676a2311ab42f5d6aa841cd00d
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-02-22 18:42:01 +0530

    Update week2q3.c

--- week2q3.c
+++ week2q3.c
@@ -1,1 +1,29 @@
-
+#include<stdio.h>
+#include<string.h>
+void main(){
+  char str[30];
+  scanf("%[^\n]s",str);
+  int start=0,end=0,count=0,length=0;
+  length=strlen(str);
+  length=length-2;
+  for(int i=length;i>=0;i--){
+    if(str[i]=='1'){
+      end=i;
+      break;
+    }
+  }
+    for(int i=0;i<length;i++){
+      if(str[i]=='1'){
+        start=i;
+        break;
+      }
+    }
+//   printf("%d %d",start,end);
+  for(int i=start;i<=end;i++){
+    if (str[i]=='0')
+    {
+        count++;
+    }    
+  }
+  printf("%d",count);
+}
