cbce00b0f088685c2ad09e6f1d81f34886ffd898
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-02-22 18:42:52 +0530

    Update week3q1.c

--- week3q1.c
+++ week3q1.c
@@ -1,1 +1,24 @@
-
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+    int arr[100];
+    int n = 0;
+    do
+    {
+        scanf("%d", &arr[n]);
+        n++;
+    } while (arr[n - 1] != -1);
+    n = n - 1;
+    for (int i = 0; i < n-1; i++)
+    {
+        printf("%0.1f",((float)(arr[i]+arr[i+1]))/2);
+        if (i<n-2)
+        {
+            printf(" ");
+        }
+        
+    }
+    
+    
+}
