4eb1760d174daefecc2f88d630f1698436830ad1
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-02-23 08:06:28 +0530

    Update week5q1.c

--- week5q1.c
+++ week5q1.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int collatz(int n,int output){
+    output++;
+    if (n==1)
+        return output;  
+    else if (n%2==0)
+        return collatz(n/2,output);
+    else
+        return collatz(3*n+1,output);
+    
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int out=collatz(n,-1);
+    printf("%d",out);
+    return 0;
+}
