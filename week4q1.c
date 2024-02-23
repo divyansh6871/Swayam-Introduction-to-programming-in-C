e3b9d2b3a774aec8536c552fa1db77666cdab8cf
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-02-23 08:04:24 +0530

    Update week4q1.c

--- week4q1.c
+++ week4q1.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    int avg=0;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+        avg+=arr[i];
+    }
+    float avg1=(float)avg/n;
+    int sum=0;
+    for (int i = 0; i < n; i++)
+    {
+        if ((float)arr[i]>=avg1)
+        {
+            sum+=arr[i];
+        }        
+    }    
+    printf("%d",sum);
+}
