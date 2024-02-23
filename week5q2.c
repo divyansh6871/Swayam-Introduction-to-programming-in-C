f9d45a08e93428be7abc140fea1bfafc9e904bf0
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-02-23 08:07:00 +0530

    Update week5q2.c

--- week5q2.c
+++ week5q2.c
@@ -1,1 +1,31 @@
-
+#include <stdio.h>
+int func(int arr[], int size)
+{
+    if (size == 2)
+    {
+        return (arr[0] - arr[1]);
+    }
+    else
+    {
+        size = size / 2;
+        int ar[size];
+        for (int i = 0; i < size; i++)
+        {
+            ar[i] = arr[2 * i] - arr[2 * i + 1];
+        }
+        func(ar,size);
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int out = func(arr, n);
+    printf("%d", out);
+    return 0;
+}
